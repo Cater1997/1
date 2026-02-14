69786f3492530c8b5a0d326d22105850d0537fcb
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-15 07:30:41 +0800

    Update diy-feeds.sh

--- diy-feeds.sh
+++ diy-feeds.sh
@@ -2,7 +2,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 拉取额外软件包
+# 拉取软件包
 git clone https://github.com/CrazyPegasus/luci-app-accesscontrol-plus.git package/luci-app-accesscontrol-plus
 git clone https://github.com/kongfl888/luci-app-adbyby-plus-lite.git package/luci-app-adbyby-plus-lite
 git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/luci-app-adguardhome
