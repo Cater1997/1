4496e65743fedd21e0b3a4bec4fb0be2ef136e59
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-16 11:54:54 +0800

    Update diy-config.sh
    Signed-off-by: Cater1997 <92653925+Cater1997@users.noreply.github.com>

--- diy-config.sh
+++ diy-config.sh
@@ -23,27 +23,21 @@ else
   find feeds/luci -name "sysauth.htm" -exec sed -i 's/value="<%=duser%>"/value="admin"/' {} \;
 fi
 
-# 编辑默认 LAN 口 IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-# 编辑默认固件名称
-#sed -i 's/ImmortalWrt/ImmortalWrt1/g' package/base-files/files/bin/config_generate
-
 # 编辑默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 取消与 E52C 无关的软件包
-sed -i 's/CONFIG_PACKAGE_arm-trusted-firmware-microchipsw=y/# CONFIG_PACKAGE_arm-trusted-firmware-microchipsw is not set/' .config
-sed -i 's/CONFIG_PACKAGE_audit=y/# CONFIG_PACKAGE_audit is not set/' .config
-sed -i 's/CONFIG_PACKAGE_auditd=y/# CONFIG_PACKAGE_auditd is not set/' .config
-sed -i 's/CONFIG_PACKAGE_autosamba=y/# CONFIG_PACKAGE_autosamba is not set/' .config
-sed -i 's/CONFIG_BUSYBOX_CONFIG_PAM=y/# CONFIG_BUSYBOX_CONFIG_PAM is not set/' .config
-sed -i 's/CONFIG_BUSYBOX_CONFIG_RPC=y/# CONFIG_BUSYBOX_CONFIG_RPC is not set/' .config
-sed -i 's/CONFIG_PACKAGE_kexec-tools=y/# CONFIG_PACKAGE_kexec-tools is not set/' .config
-sed -i 's/CONFIG_PACKAGE_lldpd=y/# CONFIG_PACKAGE_lldpd is not set/' .config
-sed -i 's/CONFIG_PACKAGE_onionshare-cli=y/# CONFIG_PACKAGE_onionshare-cli is not set/' .config
-sed -i 's/CONFIG_PACKAGE_pcat-manager=y/# CONFIG_PACKAGE_pcat-manager is not set/' .config
-sed -i 's/CONFIG_PACKAGE_policycoreutils=y/# CONFIG_PACKAGE_policycoreutils is not set/' .config
+./scripts/config --disable CONFIG_PACKAGE_arm-trusted-firmware-microchipsw
+./scripts/config --disable CONFIG_PACKAGE_audit
+./scripts/config --disable CONFIG_PACKAGE_auditd
+./scripts/config --disable CONFIG_PACKAGE_autosamba
+./scripts/config --disable CONFIG_BUSYBOX_CONFIG_PAM
+./scripts/config --disable CONFIG_BUSYBOX_CONFIG_RPC
+./scripts/config --disable CONFIG_PACKAGE_kexec-tools
+./scripts/config --disable CONFIG_PACKAGE_lldpd
+./scripts/config --disable CONFIG_PACKAGE_onionshare-cli
+./scripts/config --disable CONFIG_PACKAGE_pcat-manager
+./scripts/config --disable CONFIG_PACKAGE_policycoreutils
 
 # 修改 luci-app-amlogic 配置
 sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/ophub/kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
