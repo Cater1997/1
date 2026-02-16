8ef5ddcf5c10faa11a4f2e8defd393d3ba4e58c7
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-16 21:22:07 +0800

    Update diy-feeds.sh
    Signed-off-by: Cater1997 <92653925+Cater1997@users.noreply.github.com>

--- diy-feeds.sh
+++ diy-feeds.sh
@@ -6,22 +6,26 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # 添加第三方 feed 源
 cat >> feeds.conf.default << 'EOF'
-#src-git luci-app-parentcontrol https://github.com/sirpdboy/luci-app-parentcontrol.git;main
-#src-git luci-app-amlogic https://github.com/ophub/luci-app-amlogic.git;main
-#src-git helloworld https://github.com/fw876/helloworld.git;master
-#src-git luci-app-mosdns https://github.com/sbwml/luci-app-mosdns.git;v5
-#src-git v2ray-geodata https://github.com/sbwml/v2ray-geodata.git;master
-#src-git luci-app-netspeedtest https://github.com/sirpdboy/luci-app-netspeedtest.git;master
-#src-git openwrt-passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main
-#src-git openwrt-passwall-packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
-#src-git turboacc_luci https://github.com/chenmozhijin/turboacc.git;luci
+src-git luci-app-parentcontrol https://github.com/sirpdboy/luci-app-parentcontrol.git
+src-git luci-app-amlogic https://github.com/ophub/luci-app-amlogic.git
+src-git helloworld https://github.com/fw876/helloworld.git;master
+src-git luci-app-mosdns https://github.com/sbwml/luci-app-mosdns.git
+src-git v2ray-geodata https://github.com/sbwml/v2ray-geodata.git
+src-git luci-app-netspeedtest https://github.com/sirpdboy/luci-app-netspeedtest.git
+src-git openwrt-passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+src-git openwrt-passwall-packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git
+src-git turboacc_luci https://github.com/chenmozhijin/turboacc.git;luci
 src-git turboacc_package https://github.com/chenmozhijin/turboacc.git;package
 EOF
 
 # 拉取软件包
-#git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-#git clone https://github.com/sirpdboy/luci-app-netspeedtest.git package/luci-app-netspeedtest
-#git clone https://github.com/chenmozhijin/turboacc.git package/turboacc
+#git clone -b main --single-branch https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+#git clone -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+#git clone -b master --single-branch https://github.com/sbwml/v2ray-geodata.git package/v2ray-geodata
+#git clone -b master --single-branch https://github.com/sirpdboy/luci-app-netspeedtest.git package/luci-app-netspeedtest
+#git clone -b main --single-branch https://github.com/sirpdboy/luci-app-parentcontrol.git package/luci-app-parentcontrol
+#git clone -b main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/openwrt-passwall2
+#git clone -b main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/openwrt-passwall-packages
 
 # 解锁网易云音乐相关文件下载
 NAME=$"package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
