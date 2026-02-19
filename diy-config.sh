cf88970df791fd0e0d9953004c20e1e834e73fc0
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-19 23:00:32 +0800

    Update diy-config.sh
    Signed-off-by: Cater1997 <92653925+Cater1997@users.noreply.github.com>

--- diy-config.sh
+++ diy-config.sh
@@ -1,23 +1,23 @@
 # 设置默认主题
-./scripts/config --set-str CONFIG_LUCI_DEFAULT_THEME luci-theme-argon
+./scripts/config/conf --set-str CONFIG_LUCI_DEFAULT_THEME luci-theme-argon
 
 # 取消软件包
-./scripts/config --disable CONFIG_PACKAGE_arm-trusted-firmware-microchipsw
-./scripts/config --disable CONFIG_PACKAGE_audit
-./scripts/config --disable CONFIG_PACKAGE_auditd
-./scripts/config --disable CONFIG_PACKAGE_autosamba
-./scripts/config --disable CONFIG_BUSYBOX_CONFIG_PAM
-./scripts/config --disable CONFIG_BUSYBOX_CONFIG_RPC
-./scripts/config --disable CONFIG_PACKAGE_gst1-plugins-base
-./scripts/config --disable CONFIG_PACKAGE_kexec-tools
-./scripts/config --disable CONFIG_PACKAGE_lldpd
-./scripts/config --disable CONFIG_PACKAGE_onionshare-cli
-./scripts/config --disable CONFIG_PACKAGE_pcat-manager
-./scripts/config --disable CONFIG_PACKAGE_policycoreutils
+./scripts/config/conf --disable CONFIG_PACKAGE_arm-trusted-firmware-microchipsw
+./scripts/config/conf --disable CONFIG_PACKAGE_audit
+./scripts/config/conf --disable CONFIG_PACKAGE_auditd
+./scripts/config/conf --disable CONFIG_PACKAGE_autosamba
+./scripts/config/conf --disable CONFIG_BUSYBOX_CONFIG_PAM
+./scripts/config/conf --disable CONFIG_BUSYBOX_CONFIG_RPC
+./scripts/config/conf --disable CONFIG_PACKAGE_gst1-plugins-base
+./scripts/config/conf --disable CONFIG_PACKAGE_kexec-tools
+./scripts/config/conf --disable CONFIG_PACKAGE_lldpd
+./scripts/config/conf --disable CONFIG_PACKAGE_onionshare-cli
+./scripts/config/conf --disable CONFIG_PACKAGE_pcat-manager
+./scripts/config/conf --disable CONFIG_PACKAGE_policycoreutils
 
 # 启用外部 Rust 工具链和 build-std
-./scripts/config --enable CONFIG_RUSTC_EXTERNAL
-./scripts/config --set-str CONFIG_RUSTC "$HOME/.cargo/bin/rustc"
-./scripts/config --set-str CONFIG_CARGO "$HOME/.cargo/bin/cargo"
-./scripts/config --enable CONFIG_RUSTC_BUILD_STD
-./scripts/config --disable CONFIG_PACKAGE_rust
+./scripts/config/conf --enable CONFIG_RUSTC_EXTERNAL
+./scripts/config/conf --set-str CONFIG_RUSTC "$HOME/.cargo/bin/rustc"
+./scripts/config/conf --set-str CONFIG_CARGO "$HOME/.cargo/bin/cargo"
+./scripts/config/conf --enable CONFIG_RUSTC_BUILD_STD
+./scripts/config/conf --disable CONFIG_PACKAGE_rust
