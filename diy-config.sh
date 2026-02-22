199ce57637113d26203b585fd823eb38f451386c
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-22 20:16:16 +0800

    Update diy-config.sh
    Signed-off-by: Cater1997 <92653925+Cater1997@users.noreply.github.com>

--- diy-config.sh
+++ diy-config.sh
@@ -14,26 +14,3 @@ sed -i 's/^CONFIG_PACKAGE_lldpd=.*/# CONFIG_PACKAGE_lldpd is not set/' .config
 sed -i 's/^CONFIG_PACKAGE_onionshare-cli=.*/# CONFIG_PACKAGE_onionshare-cli is not set/' .config
 sed -i 's/^CONFIG_PACKAGE_pcat-manager=.*/# CONFIG_PACKAGE_pcat-manager is not set/' .config
 sed -i 's/^CONFIG_PACKAGE_policycoreutils=.*/# CONFIG_PACKAGE_policycoreutils is not set/' .config
-
-# 启用外部 Rust 工具链
-if grep -q "^CONFIG_RUSTC_EXTERNAL=" .config; then
-    sed -i 's/^CONFIG_RUSTC_EXTERNAL=.*/CONFIG_RUSTC_EXTERNAL=y/' .config
-else
-    echo "CONFIG_RUSTC_EXTERNAL=y" >> .config
-fi
-if grep -q "^CONFIG_RUSTC=" .config; then
-    sed -i "s|^CONFIG_RUSTC=.*|CONFIG_RUSTC=\"$HOME/.cargo/bin/rustc\"|" .config
-else
-    echo "CONFIG_RUSTC=\"$HOME/.cargo/bin/rustc\"" >> .config
-fi
-if grep -q "^CONFIG_CARGO=" .config; then
-    sed -i "s|^CONFIG_CARGO=.*|CONFIG_CARGO=\"$HOME/.cargo/bin/cargo\"|" .config
-else
-    echo "CONFIG_CARGO=\"$HOME/.cargo/bin/cargo\"" >> .config
-fi
-if grep -q "^CONFIG_RUSTC_BUILD_STD=" .config; then
-    sed -i 's/^CONFIG_RUSTC_BUILD_STD=.*/CONFIG_RUSTC_BUILD_STD=y/' .config
-else
-    echo "CONFIG_RUSTC_BUILD_STD=y" >> .config
-fi
-sed -i 's/^CONFIG_PACKAGE_rust=.*/# CONFIG_PACKAGE_rust is not set/' .config
