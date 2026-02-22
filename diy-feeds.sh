b689810ba96671bca1435091c1f2affe7fa5d3c7
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-22 20:15:26 +0800

    Update diy-feeds.sh
    Signed-off-by: Cater1997 <92653925+Cater1997@users.noreply.github.com>

--- diy-feeds.sh
+++ diy-feeds.sh
@@ -4,37 +4,13 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 编辑默认固件名称
 #sed -i 's/ImmortalWrt/ImmortalWrt1/g' package/base-files/files/bin/config_generate
 
-# 添加第三方 feed 源
+# 添加 feed 源
 cat > feeds.conf.default << 'EOF'
 src-git packages https://github.com/immortalwrt/packages.git;master
 src-git luci https://github.com/immortalwrt/luci.git;master
 src-git routing https://github.com/openwrt/routing.git;master
-src-git helloworld https://github.com/fw876/helloworld.git;master
 EOF
-
-# 拉取软件包
-git clone -b main --single-branch https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-git clone -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns.git package/mosdns
-git clone -b master --single-branch https://github.com/sbwml/v2ray-geodata.git package/v2ray-geodata
-git clone -b master --single-branch https://github.com/sirpdboy/luci-app-netspeedtest.git package/netspeedtest
-git clone -b main --single-branch https://github.com/sirpdboy/luci-app-parentcontrol.git package/luci-app-parentcontrol
-git clone -b main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/passwall2
-git clone -b main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall-packages
-
-# 修改 luci-app-amlogic 配置
-sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/ophub/kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
-sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 
 # 添加turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 rm -f add_turboacc.sh
-
-# 其他调整
-NAME=$"package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
-curl 'https://api.github.com/repos/UnblockNeteaseMusic/server/commits?sha=enhanced&path=precompiled' -o commits.json
-echo "$(grep sha commits.json | sed -n "1,1p" | cut -c 13-52)">"$NAME/core_local_ver"
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/bridge.js -o $NAME/core/bridge.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/ca.crt -o $NAME/core/ca.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key
