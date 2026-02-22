5eb05d7e23220f2c3665fba7e287bd5d1fcbce38
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-22 21:55:18 +0800

    Update diy-config.sh
    Signed-off-by: Cater1997 <92653925+Cater1997@users.noreply.github.com>

--- diy-config.sh
+++ diy-config.sh
@@ -14,3 +14,19 @@ sed -i 's/^CONFIG_PACKAGE_lldpd=.*/# CONFIG_PACKAGE_lldpd is not set/' .config
 sed -i 's/^CONFIG_PACKAGE_onionshare-cli=.*/# CONFIG_PACKAGE_onionshare-cli is not set/' .config
 sed -i 's/^CONFIG_PACKAGE_pcat-manager=.*/# CONFIG_PACKAGE_pcat-manager is not set/' .config
 sed -i 's/^CONFIG_PACKAGE_policycoreutils=.*/# CONFIG_PACKAGE_policycoreutils is not set/' .config
+
+# 设置外部 Rust 工具链
+sed -i '/CONFIG_RUSTC_EXTERNAL[ =]/d' .config
+echo 'CONFIG_RUSTC_EXTERNAL=y' >> .config
+
+sed -i '/CONFIG_RUSTC[ =]/d' .config
+echo "CONFIG_RUSTC=\"$HOME/.cargo/bin/rustc\"" >> .config
+
+sed -i '/CONFIG_CARGO[ =]/d' .config
+echo "CONFIG_CARGO=\"$HOME/.cargo/bin/cargo\"" >> .config
+
+sed -i '/CONFIG_RUSTC_BUILD_STD[ =]/d' .config
+echo 'CONFIG_RUSTC_BUILD_STD=y' >> .config
+
+sed -i '/CONFIG_PACKAGE_rust[ =]/d' .config
+echo '# CONFIG_PACKAGE_rust is not set' >> .config
